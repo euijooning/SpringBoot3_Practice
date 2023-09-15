111632b9b68793da4545105d0fb7949e422a711e
EUIJOON <euijoonism@gmail.com>
2023-09-15 19:17:50 +0900

    7장. 블로그 화면 구성하기 -  3. 블로그 글 뷰 구현하기

--- src/main/java/boot3/jwtAction/controller/BlogViewController.java
+++ src/main/java/boot3/jwtAction/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package boot3.jwtAction.controller;
 
+import boot3.jwtAction.domain.Article;
 import boot3.jwtAction.dto.ArticleListViewResponse;
+import boot3.jwtAction.dto.ArticleViewResponse;
 import boot3.jwtAction.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -29,5 +32,15 @@ public class BlogViewController {
 
         // "articleList" 뷰를 반환
         return "articleList"; // 뷰 조회
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        // 요청된 ID에 해당하는 블로그 글을 블로그 서비스를 통해 조회
+        Article article = blogService.findById(id);
+        // 조회한 블로그 글을 "article"라는 이름으로 모델에 추가
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article"; // "article" 뷰를 반환
     }
 }
