a779d83d56b7936b7f7460edcd8bbe4887b48456
EUIJOON <euijoonism@gmail.com>
2023-09-15 15:48:34 +0900

    패키지명 변경

--- src/main/java/boot3/Boot3Application.java
+++ src/main/java/boot3/Boot3Application.java
@@ -1,4 +1,4 @@
-package jwt;
+package boot3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/boot3/Boot3ApplicationTests.java
+++ src/test/java/boot3/Boot3ApplicationTests.java
@@ -1,4 +1,4 @@
-package jwt;
+package boot3;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
