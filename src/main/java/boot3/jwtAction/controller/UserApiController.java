af8a5d6d847da13cc90a6e321492bdecd2dc19d4
EUIJOON <euijoonism@gmail.com>
2023-09-15 20:24:26 +0900

    8장. 시큐리티 - 로그인/로그아웃/회원가입 - 4. 로그아웃 구현하기 (뷰는 이미 구현함)

--- src/main/java/boot3/jwtAction/controller/UserApiController.java
+++ src/main/java/boot3/jwtAction/controller/UserApiController.java
@@ -2,8 +2,13 @@ package boot3.jwtAction.controller;
 
 import boot3.jwtAction.dto.request.AddUserRequest;
 import boot3.jwtAction.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -18,4 +23,13 @@ public class UserApiController {
         return "redirect:/login";
     }
 
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        // SecurityContextLogoutHandler를 사용하여 사용자 로그아웃 처리를 수행
+        new SecurityContextLogoutHandler()
+                .logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+
+        // 로그아웃 처리 후, "/login" 경로로 리디렉션
+        return "redirect:/login";
+    }
 }
