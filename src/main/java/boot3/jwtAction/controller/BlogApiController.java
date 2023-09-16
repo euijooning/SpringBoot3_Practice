ed5ff03efd2a06112932e445301e580fd07b83d1
EUIJOON <euijoonism@gmail.com>
2023-09-16 13:13:45 +0900

    10장. OAuth - 4.글에 글쓴이 추가하기

--- src/main/java/boot3/jwtAction/controller/BlogApiController.java
+++ src/main/java/boot3/jwtAction/controller/BlogApiController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -18,14 +19,15 @@ public class BlogApiController {
 
     private final BlogService blogService;
 
+
     @PostMapping("/api/articles")
     //요청 본문 값 매핑
     // HTTP POST 요청을 처리하여 블로그 글을 추가합니다.
-    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
-        Article savedArticle = blogService.save(request);
-
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request, Principal principal) {
+        Article savedArticle = blogService.save(request, principal.getName());
         // 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedArticle);
     }
 
     @GetMapping("/api/articles")
